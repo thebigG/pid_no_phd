fa51f0a51cfc544063298e0f640e05c1f6523cb1
thebigG <yunior.eury@gmail.com>
2024-02-13 08:15:36 -0600

    -Add get_temp_for_time_step. WIP.

--- proportional/src/main.rs
+++ proportional/src/main.rs
@@ -5,13 +5,32 @@ struct Temperature
     t2: f32,
     th: f32,
     ta: f32,
-    vd: f32
+    vd: f32,
+    kh: f32
+}
+
+impl Temperature {
+    fn get_temp_for_time_step(self) -> f64
+    {
+    //     Add formula here.
+
+        return 0.0
+    }
 }
 
 fn main() {
     let timer = timer::Timer::new();
-    timer.schedule_repeating(chrono::Duration::milliseconds(5), move || {
-    // *count.lock().unwrap() += 1;
-        println!("Hello, world!");
-  });
+    let t: Temperature = Temperature{
+        t1: 0.0,
+        t2: 0.0,
+        th: 0.0,
+        ta: 0.0,
+        vd: 0.0,
+        kh: 0.0,
+    };
+    t.get_temp_for_time_step();
+  //   timer.schedule_repeating(chrono::Duration::milliseconds(5), move || {
+  //   // *count.lock().unwrap() += 1;
+  //       println!("Hello, world!");
+  // });
 }
